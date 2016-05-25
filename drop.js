a3c6de5bf1bfcd8ca85ffd053e0923c67bc52003
dfenjves <fenjves.daniel@gmail.com>
2016-05-24 22:22:01 -0400

    color

--- drop.js
+++ drop.js
@@ -1,4 +1,3 @@
-
 function Drop(){
   
   this.x = random(width);
@@ -7,20 +6,29 @@ function Drop(){
   this.gravity = map(this.z,0,20,0.01,0.2);
   this.length = map(this.z,0,20,10,30);
   this.speed = map(this.z, 0, 20, 3,10);
+  this.dropColor = color(random(255),random(255),random(255));
 
   this.fall = function(){
 
     this.y += this.speed;
     this.speed += this.gravity;
+
     if (this.y > height){
+      console.log("now");
+      dropSound.playMode('sustain');
+      dropSound.play();
       this.y = random(-100,-200);
       this.speed = map(this.z, 0, 20, 3,10);
     }
   }
 
   this.show = function(){
-    console.log(this.y);
-    stroke(136, 43, 226);
+    if(mouseIsPressed){
+      stroke(this.dropColor);
+    }else{
+      stroke(136, 43, 226);
+    }
+      
     strokeWeight(map(this.z, 0, 20, 1, 3));
     line(this.x, this.y, this.x, this.y + this.length);
   }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 var newDrops;
+var dropSound;
+
+function preload(){
+  dropSound = loadSound('dropSound.mp3');
+  dropSound.setVolume(0.1);
+}
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   newDrops = [];
-  for (var i = 0; i < 250; i++) {
+  for (var i = 0; i < 5; i++) {
     newDrops.push(new Drop());
   };
 }
